591960d32bb9878969eda9c586b47de725b37903
Thiago Esteves <thiago.esteves@erlang-solutions.com>
2025-03-22 13:25:31 -0300

    Adding multiple models for analysis

--- lib/live_picture/pictures/picture.ex
+++ lib/live_picture/pictures/picture.ex
@@ -16,17 +16,27 @@ defmodule LivePicture.Pictures.Picture do
     :duration
   ]
 
+  @all_models [
+    :alexnet,
+    :convnext,
+    :resnet18,
+    :squeezenet,
+    :vgg16,
+    :densenet121,
+    :efficientnet,
+    :mobilenet,
+    :regnet,
+    :vision_transformer,
+    :swin_transformer,
+    :inception
+  ]
+
   schema "pictures" do
     field(:name, :string)
     field(:path, :string)
     field(:prediction, :string, default: "-/-")
     field(:duration, :integer, default: 0)
-
-    field(:model, Ecto.Enum,
-      values: [:alexnet, :convnext, :resnet18, :squeezenet, :vgg16],
-      default: :alexnet
-    )
-
+    field(:model, Ecto.Enum, values: @all_models, default: :alexnet)
     field(:upload_status, Ecto.Enum, values: [:uploading, :uploaded])
 
     field(:analysis_status, Ecto.Enum,
--- lib/live_picture_web/live/picture/components/model_selection.ex
+++ lib/live_picture_web/live/picture/components/model_selection.ex
@@ -51,9 +51,14 @@ defmodule LivePictureWeb.Components.ModelSelection do
   defp model_label(assigns) do
     ~H"""
     <div class="inline-flex items-center justify-between w-full p-2 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
-      <div class="w-full font-mono text-l">{String.upcase("#{@model.name}")}</div>
+      <div class="w-full font-mono text-xs">{normalize_model_text("#{@model.name}")}</div>
     </div>
     """
+  end
+
+  defp normalize_model_text(text) do
+    [head | _rest] = String.split(text, "_")
+    String.upcase(head)
   end
 
   defp selected_button_class("on"),
